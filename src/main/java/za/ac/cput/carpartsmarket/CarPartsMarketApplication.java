cce4ea04c7d298fb9eccf4a5a9e89c31510be5e0
InamKlaas <222304502@mycput.ac.za>
2026-06-20 21:05:45 +0200

    base of the project

---
+++ src/main/java/za/ac/cput/carpartsmarket/CarPartsMarketApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.carpartsmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPartsMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarPartsMarketApplication.class, args);
+    }
+
+}
